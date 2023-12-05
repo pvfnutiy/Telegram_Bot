ef62af8243cd69060aad141d09fbd4601d086736
pafnutiy <105767751+pvfnutiy@users.noreply.github.com>
2023-12-05 22:35:25 +0700

    Parsing update
    Позволяет парсить цены на некоторых объявлениях маркетплейсов

--- telegram_bot/parser.py
+++ telegram_bot/parser.py
@@ -5,19 +5,10 @@ import json
 class ozon_parsing():
     def __init__(self, url:str):
         self.url = url
-        self.apiurl = "https://www.ozon.ru/api/entrypoint-api.bx/page/json/v2?url=%2F"
-        self.rawurl = self.url.split('/')
         self.driver = webdriver.Chrome()
-
-    def get_api_url(self):
-        for i in range(0, 3):
-            del self.rawurl[i]
-        del self.rawurl[0]
-        self.rawurl = '/'.join(self.rawurl)
-        return self.apiurl + self.rawurl
 
     def get_info(self, url:str):
         self.src = self.driver.get(url)
         self.srcdata = self.driver.page_source
-        self.result = BeautifulSoup(self.srcdata, 'html.parser').find("thinsp", class_="15m ml3")
-        print(self.result)
+        self.soup = BeautifulSoup(self.srcdata, 'html.parser').find('span', attrs={'class': {'lm7 l5m','ml3 m1l'}})
+        return str(self.soup.get_text())
