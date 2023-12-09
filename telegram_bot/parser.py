b29875e64994a4493ce7a7ac229251533dcc25c3
pafnutiy <105767751+pvfnutiy@users.noreply.github.com>
2023-12-09 17:53:11 +0700

    Parsing update
    Теперь с карточки товара можно достать все имеющиеся цены и использовать их как будет угодно.
    Пока что getting_info просто возвращает базовое сообщение о имеющихся ценах

--- telegram_bot/parser.py
+++ telegram_bot/parser.py
@@ -1,6 +1,6 @@
 from bs4 import BeautifulSoup
 from selenium import webdriver
-import json
+import re
 
 class ozon_parsing():
     def __init__(self, url:str):
@@ -8,7 +8,20 @@ class ozon_parsing():
         self.driver = webdriver.Chrome()
 
     def get_info(self, url:str):
+        self.Data = []
         self.src = self.driver.get(url)
         self.srcdata = self.driver.page_source
-        self.soup = BeautifulSoup(self.srcdata, 'html.parser').find('span', attrs={'class': {'lm7 l5m','ml3 m1l'}})
-        return str(self.soup.get_text())
+        self.soup = BeautifulSoup(self.srcdata, 'html.parser').findAll('div', attrs={'class': {'m4l'}})
+        self.result = ''.join(str(self.soup[0]))
+        self.result = re.split(">", self.result)
+
+        for i in self.result:
+            for j in i:
+                if j == '₽':
+                    self.Data.append(i)
+        
+        self.Data = ''.join(self.Data)
+        self.Data = re.split("<"+"/span", self.Data)
+
+        print(' '.join(self.Data))
+        return f'Цена с озон-картой: {self.Data[0]}\nЦена без озон карты: {self.Data[1]}\nСтарая цена: {self.Data[2]}'
